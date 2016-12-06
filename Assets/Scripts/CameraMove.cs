9a3e7c34a45597a95172bb15ef6719acfb73ef61
ryonakao <rodriguez.nak@gmail.com>
2016-12-06 15:16:01 +0900

    移動完了

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + new Vector3 (0, 9, -13);
+	}
+}
---
+++ Assets/Scripts/PlayreMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayreMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+	//移動する
+	void Move()
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += transform.forward;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= transform.forward;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += transform.right;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position -= transform.right;
+		}
+	}
+}
