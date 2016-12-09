df11043853d7e181d2ddd7892e7e92ff24aa6b94
ryonakao <rodriguez.nak@gmail.com>
2016-12-09 20:12:33 +0900

    パラメータの設定

---
+++ Assets/Scripts/ParameterManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterManager : SingletonMonoBehaviour<ParameterManager> {
+	//剣の長さ
+	public int sordLength;
+	//スピード
+	public int speed;
+
+	void Start()
+	{
+		Initialize ();
+		Get ();
+	}
+	//キーを初期化
+	void Initialize()
+	{
+		if (!PlayerPrefs.HasKey ("SORDLENGTH"))
+		{
+			PlayerPrefs.SetInt("SORDLENGTH",1);
+		}
+		if (!PlayerPrefs.HasKey ("SPEED"))
+		{
+			PlayerPrefs.SetInt("SPEED",1);
+		}
+	}
+	//キーに保存されている値を取得
+	void Get()
+	{
+		sordLength = PlayerPrefs.GetInt ("SORDLENGTH");
+		speed = PlayerPrefs.GetInt ("SPEED");
+	}
+	//保存
+	void Save()
+	{
+		PlayerPrefs.SetInt ("SORDLENGTH",sordLength);
+		PlayerPrefs.SetInt ("SPEED",speed);
+	}
+}
--- Assets/Scripts/ResultUIManager.cs
+++ Assets/Scripts/ResultUIManager.cs
@@ -7,6 +7,7 @@ public class ResultUIManager : MonoBehaviour {
 
 	public Text score;
 
+	public Text experience;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,11 +16,18 @@ public class ResultUIManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Score ();
+		Experience ();
 	}
 	//スコアを表示
 	void Score()
 	{
 		score.text = ScoreManager.Instance.killedEnemy.ToString ();
+	}
+		
+	//経験値を表示
+	void Experience()
+	{
+		experience.text = ExperienceManager.Instance.experience.ToString ();
 	}
 	//リトライ
 	public void Retry()
