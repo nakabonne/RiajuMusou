a87704d8449caf4bbba6c839b7efb2e6b906cb84
ryonakao <rodriguez.nak@gmail.com>
2016-12-14 01:37:11 +0900

    敵のアニメーション完了

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -13,6 +13,6 @@ public class CameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + new Vector3 (0, 15, -11);
+		transform.position = player.transform.position + new Vector3 (0, 12, -11);
 	}
 }
--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
 
-	float speed = 0.05f;
+	float speed = 0.2f;
 
 	// Use this for initialization
 	void Start () {
