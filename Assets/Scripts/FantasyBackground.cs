545e0884a0aab3c7b6847d3a0c7dbdf7d60281eb
ryonakao <rodriguez.nak@gmail.com>
2016-12-18 15:15:16 +0900

    prepareまでUI完了

---
+++ Assets/Scripts/FantasyBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FantasyBackground : MonoBehaviour {
+	public static FantasyBackground instance = null;
+
+
+	void Awake(){
+
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
